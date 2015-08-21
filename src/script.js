3a49ceb0add3b0174fa81e3704944d05aa54f2af
mebrunet <marcetiennebrunet@gmail.com>
2015-08-21 16:47:12 -0400

    Cleaner css; show/hide password; allows no callbacks

--- src/script.js
+++ src/script.js
@@ -1,6 +1,4 @@
 /*
-All this JS will be injected inplace of
-<script src='script.js'></script>
 
 IMPORTANT file must NOT contain any double quotes (').
 
@@ -11,13 +9,15 @@ IMPORTANT file must NOT contain any double quotes (').
 var base_url = 'http://192.168.0.1/';
 var network_list;
 var public_key;
-var rsa = new RSAKey();
+var rsa = new RSAKey(); // requires rsa-utils/
 
 // Common DOM elements
 var scanButton = document.getElementById('scan-button');
 var connectButton = document.getElementById('connect-button');
 var copyButton = document.getElementById('copy-button');
+var showButton = document.getElementById('show-button');
 var deviceID = document.getElementById('device-id');
+var connectForm = document.getElementById('connect-form');
 
 
 // Function ordered by typical user flow ---------------------------------------
@@ -67,25 +67,23 @@ var scan_callback = {
     console.log('I found:');
     var networks_div = document.getElementById('networks-div');
     networks_div.innerHTML = ''; //start by clearing html
-    var form = document.createElement('FORM')
     
     if(network_list.length > 0){
       for(var i=0; i < network_list.length; i++){
         ssid = network_list[i]['ssid'];
         console.log(network_list[i]);
-        add_wifi_option(form, ssid);
+        add_wifi_option(networks_div, ssid);
         // Show password and connect
         document.getElementById('connect-div').style.display = 'block';
       }
-      networks_div.appendChild(form);
     } else {
-      networks_div.innerHTML = '<p>No networks found.</p>'; //start by clearing html
+      networks_div.innerHTML = '<p class=\'scanning-error\'>No networks found.</p>';
     }
   },
 
   error: function(error){
     console.log('Scanning error:' + error);
-    document.getElementById('networks-div').innerHTML = '<p>Scanning error.</p>';
+    document.getElementById('networks-div').innerHTML = '<p class=\'scanning-error\'>Scanning error.</p>';
   },
 
   regardless: function(){
@@ -95,14 +93,15 @@ var scan_callback = {
   }
 };
 
-var configure = function(){ 
+var configure = function(evt){
+  evt.preventDefault();
   // get user input
   var network = get_selected_network();
   var password = document.getElementById('password').value;
   // simple validation
   if(!network){
     window.alert('Please select a network!');
-    return;
+    return false;
   }
   // prep payload
   var jsonData = {
@@ -112,13 +111,13 @@ var configure = function(){
     sec: network.sec,
     ch: network.ch
 
-  }
+  };
   // send
-  connectButton.innerHTML = 'Sending credentials...';
+  connectButton.innerHTML = 'Sending...';
   disableButtons();
   console.log('Sending credentials: ' + JSON.stringify(jsonData));
   postRequest(base_url+'configure-ap', jsonData, configure_callback);
-}
+};
 
 var configure_callback = {
   success: function(resp){
@@ -127,29 +126,27 @@ var configure_callback = {
     //postRequest(base_url+'connect-ap', {idx:0});
     getRequest(base_url+'connect'); // strangely this works more consistently...
     connectButton.innerHTML = 'Connecting to cloud...';
-    window.alert('Your device should now start flashing and attempt to connect to the cloud. This usually takes about 20 seconds, after which it will begin \'breathing\' (i.e. slowly blinking) cyan. \n\n\nIf nothing happens, and your device stays blue, push the reset button to trigger the connection process. \n\n\nIf this process fails because you entered to wrong password the device will flash green indefinitely. In this case, hold the setup button for 6 seconds (until the device starts blinking blue again), reconnect to the access point (WiFi hotspot) it generates, and reload this page to try again.');
+    window.alert('Your device should now start flashing and attempt to connect to the cloud. This usually takes about 20 seconds, after which it will begin \'breathing\' (i.e. slowly blinking) cyan. \n\n\nIf nothing happens, and your device stays blue, push the reset button to trigger the connection process. \n\n\nIf this process fails because you entered the wrong password, the device will flash green indefinitely. In this case, hold the setup button for 6 seconds (until the device starts blinking blue again), reconnect to the access point (WiFi hotspot) it generates, and reload this page to try again.');
   },
   error: function(error, resp){
     console.log('Credential error: ' + error);
-    window.alert('The connection command failed, check that you are still well connected to the device\'s access point (WiFi hotspot) and retry.')
+    window.alert('The connection command failed, check that you are still well connected to the device\'s access point (WiFi hotspot) and retry.');
     connectButton.innerHTML = 'Retry';
     enableButtons();
   }
-}
+};
 
 // Helper methods --------------------------------------------------------------
 
 var disableButtons = function (){
   connectButton.disabled = true;
   scanButton.disabled = true;
-  copyButton.disabled = true;
-}
+};
 
 var enableButtons = function (){
   connectButton.disabled = false;
   scanButton.disabled = false;
-  copyButton.disabled = false;
-}
+};
 
 var add_wifi_option = function(parent, ssid){
   var radio = document.createElement('INPUT');
@@ -157,13 +154,16 @@ var add_wifi_option = function(parent, ssid){
   radio.value = ssid;
   radio.id = ssid;
   radio.name = 'ssid';
-  parent.appendChild(document.createElement('BR'));
-  parent.appendChild(radio);
-  var label = document.createElement('SPAN');
-  label.className = 'ssid-label';
+  radio.className = 'radio' ;
+  var div = document.createElement('DIV');
+  div.className = 'radio-div';
+  div.appendChild(radio);
+  var label = document.createElement('label');
+  label.htmlFor = ssid;
   label.innerHTML = ssid;
-  parent.appendChild(label);
-}
+  div.appendChild(label);
+  parent.appendChild(div);
+};
 
 var get_selected_network = function(){
   // network_list is global
@@ -173,36 +173,51 @@ var get_selected_network = function(){
       return network_list[i];
     }
   }
-}
+};
 
-var copy = function copy() {
+var copy = function() {
   window.prompt('Copy to clipboard: Ctrl + C, Enter', deviceID.value);
-}
+};
+
+var toggleShow = function(){
+  var passwordInput = document.getElementById('password');
+  inputType = passwordInput.type;
+  
+  if(inputType === 'password'){
+    showButton.innerHTML = 'Hide';
+    passwordInput.type = 'text';
+  } else {
+    showButton.innerHTML = 'Show';
+    passwordInput.type = 'password';
+  }
+};
 
 var getRequest = function(url, callback){
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open('GET', url, true); //true specifies async
-  xmlhttp.timeout = 8000; // Long timeout needed for scan-ap 
+  xmlhttp.timeout = 8000; // Long timeout needed for scan-ap
   xmlhttp.send();
   xmlhttp.onreadystatechange = function(){
     if (xmlhttp.readyState==4){
-      if(xmlhttp.status==200){
-        //Response okay
-        if(callback.success){
-          callback.success(JSON.parse(xmlhttp.responseText));
+      if(callback){
+        if(xmlhttp.status==200){
+          //Response okay
+          if(callback.success){
+            callback.success(JSON.parse(xmlhttp.responseText));
+          }
+        } else {
+          //Error
+          if(callback.error){
+            callback.error(xmlhttp.status, xmlhttp.responseText);
+          }
         }
-      } else {
-        //Error
-        if(callback.error){  
-          callback.error(xmlhttp.status, xmlhttp.responseText);
+        if (callback.regardless){
+          //executed regardless
+          callback.regardless();
         }
-      }
-      if (callback.regardless){
-        //executed regardless
-        callback.regardless();
       }
     }
-  }
+  };
 };
 
 var postRequest = function(url, jsonData, callback){
@@ -218,23 +233,25 @@ var postRequest = function(url, jsonData, callback){
   // Handle response
   xmlhttp.onreadystatechange = function(){
     if (xmlhttp.readyState==4){
-      if(xmlhttp.status==200){
-        //Response okay
-        if(callback.success){
-          callback.success(JSON.parse(xmlhttp.responseText));
+      if(callback){
+        if(xmlhttp.status==200){
+          //Response okay
+          if(callback.success){
+            callback.success(JSON.parse(xmlhttp.responseText));
+          }
+        } else {
+          //Error
+          if(callback.error){
+            callback.error(xmlhttp.status, xmlhttp.responseText);
+          }
         }
-      } else {
-        //Error
-        if(callback.error){
-          callback.error(xmlhttp.status, xmlhttp.responseText);
+        //executed regardless
+        if (callback.regardless){
+          callback.regardless();
         }
-      }
-      //executed regardless
-      if (callback.regardless){
-        callback.regardless();
       }
     }
-  }
+  };
 };
 
 // Executed immediately on load -----------------------------------------------
@@ -242,12 +259,14 @@ var postRequest = function(url, jsonData, callback){
 // Attach events
 if (scanButton.addEventListener) {  // For all major browsers
     copyButton.addEventListener('click', copy);
+    showButton.addEventListener('click', toggleShow);
     scanButton.addEventListener('click', scan);
-    connectButton.addEventListener('click', configure);
+    connectForm.addEventListener('submit', configure);
 } else if (scanButton.attachEvent) { // For IE 8 and earlier
     copyButton.attachEvent('onclick', copy);
+    showButton.attachEvent('onclick', toggleShow);
     scanButton.attachEvent('onclick', scan);
-    connectButton.attachEvent('onclick', configure);
+    connectForm.attachEvent('onsubmit', configure);
 }
 
 // Get important device information
